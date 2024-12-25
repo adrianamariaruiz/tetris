d2d0c5968769c57f4b97a4fc7c7407a937e084f0
Adriana <adriannaruiz24@gmail.com>
2024-12-25 16:31:46 -0500

    with collisions  and solidify

--- src/main.js
+++ src/main.js
@@ -1,24 +1,137 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+//inicializar el canvas
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
 
-setupCounter(document.querySelector('#counter'))
+const BLOCK_SIZE = 20
+const BOARD_WIDTH = 14
+const BOARD_HEIGHT = 30
+
+canvas.width = BLOCK_SIZE * BOARD_WIDTH
+canvas.height = BLOCK_SIZE * BOARD_HEIGHT
+
+context.scale(BLOCK_SIZE, BLOCK_SIZE)
+
+//board
+const board = [
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [1,1,1,1,1,1,1,1,0,0,0,1,1,1],
+]
+
+//Pieza del jugador
+const piece = {
+  position: {x: 5, y:5},
+  shape: [
+    [1,1],
+    [1,1]
+  ]
+}
+
+//game loop
+function update (){
+  draw()
+  window.requestAnimationFrame(update)
+}
+function draw() {
+  context.fillStyle = '#0129'
+  context.fillRect(0, 0, canvas.width, canvas.height)
+
+  board.forEach((row, y)=> {
+    row.forEach((value, x)=>{
+      if(value === 1){
+        context.fillStyle = 'yellow'
+        context.fillRect(x, y, 1, 1)
+      }
+    })
+  })
+
+  piece.shape.forEach((row, y)=>{
+    row.forEach((value, x)=>{
+      if(value === 1){
+        context.fillStyle = 'red'
+        context.fillRect(x+piece.position.x, y+piece.position.y, 1, 1)
+      }
+    })
+  })
+}
+
+//position de la pieza
+document.addEventListener('keydown', event =>{
+  if(event.key === 'ArrowLeft') {
+    piece.position.x--
+    if(checkCollision()){
+      piece.position.x++
+    }
+  }
+  if(event.key === 'ArrowRight'){
+    piece.position.x++
+    if(checkCollision()){
+      piece.position.x--
+    }
+  } 
+  if(event.key === 'ArrowDown') {
+    piece.position.y++
+    if(checkCollision()){
+      piece.position.y--
+      solidifyPiece()
+    }
+  }
+})
+
+//colisiones
+function checkCollision() {
+  return piece.shape.find((row, y)=>{
+    return row.find((value, x)=>{
+      return (
+        value !== 0 &&
+        board[y + piece.position.y]?.[x + piece.position.x] !== 0
+      )
+    })
+  })
+}
+
+//solidificar las piezas en el tablero
+function solidifyPiece() {
+  piece.shape.forEach((row, x)=>{
+    row.forEach((value, y) => {
+      if(value === 1){
+        board[y + piece.position.y][x + piece.position.x] = 1
+      }
+    })
+  })
+
+  piece.position.x = 0
+  piece.position.y = 0
+}
+
+update()
+
