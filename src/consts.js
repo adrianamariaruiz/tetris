fb94bc44a6fe4fc6626e84f55d532c308d5a3639
Adriana <adriannaruiz24@gmail.com>
2025-03-11 09:01:18 -0500

    fix/constants

---
+++ src/consts.js
@@ -0,0 +1,31 @@
+export const BLOCK_SIZE = 20
+export const BOARD_WIDTH = 14
+export const BOARD_HEIGHT = 30
+
+// Random piece
+export const PIECES = [
+  [
+    [1,1],
+    [1,1]
+  ],
+  [
+    [1,1,1,1]
+  ],
+  [
+    [0,1,0],
+    [1,1,1]
+  ],
+  [
+    [1,1,0],
+    [0,1,1]
+  ],
+  [
+    [0,1,1],
+    [1,1,0]
+  ],
+  [
+    [1,0],
+    [1,0],
+    [1,1]
+  ]
+]
